cb7e76bc4ef865aff1f6f88d982ff55cf14ac388
André Freitas <p.andrefreitas@gmail.com>
2012-03-17 20:06:31 +0000

    Completed additional exercise with materials application.

--- tp2/code/src/LightingScene.cpp
+++ tp2/code/src/LightingScene.cpp
@@ -7,53 +7,51 @@
 #include <math.h>
 
 float pi = acos(-1.0);
-float deg2rad=pi/180.0;
+float deg2rad = pi / 180.0;
 
 #define BOARD_HEIGHT 6.0
 #define BOARD_WIDTH 6.4
 
 // Positions for two lights
-float light0_pos[4] = {4, 6.0, 1.0, 1.0};
-float light1_pos[4] = {10.5, 6.0, 1.0, 1.0};
+float light0_pos[4] = { 4, 6.0, 1.0, 1.0 };
+float light1_pos[4] = { 10.5, 6.0, 1.0, 1.0 };
 
-float light2_pos[4] = {10.5, 6.0, 5.0, 1.0};
-float light3_pos[4] = {4, 6.0, 5.0, 1.0};
-
+float light2_pos[4] = { 10.5, 6.0, 5.0, 1.0 };
+float light3_pos[4] = { 4, 6.0, 5.0, 1.0 };
 
 // Global ambient light (do not confuse with ambient component of individual lights)
-float globalAmbientLight[4]= {0,0,0,0};
+float globalAmbientLight[4] = { 0, 0, 0, 0 };
 
 // number of divisions
 #define BOARD_A_DIVISIONS 30
 #define BOARD_B_DIVISIONS 100
 
 // Coefficients for material A
-float ambA[3] = {0.2, 0.2, 0.2};
-float difA[3] = {0.6, 0.6, 0.6};
-float specA[3] = {0, 0.8, 0.8};
+float ambA[3] = { 0.2, 0.2, 0.2 };
+float difA[3] = { 0.6, 0.6, 0.6 };
+float specA[3] = { 0, 0.8, 0.8 };
 float shininessA = 120.f;
 
 // Coefficients for material B
-float ambB[3] = {0.2, 0.2, 0.2};
-float difB[3] = {0.6, 0.6, 0.6};
-float specB[3] = {0.8, 0.8, 0.8};
+float ambB[3] = { 0.2, 0.2, 0.2 };
+float difB[3] = { 0.6, 0.6, 0.6 };
+float specB[3] = { 0.8, 0.8, 0.8 };
 float shininessB = 120.f;
 
-float ambientNull[4]={0,0,0,1};
-float yellow[4]={1,1,0,1};
+float ambientNull[4] = { 0, 0, 0, 1 };
+float yellow[4] = { 1, 1, 0, 1 };
 
-void LightingScene::init() 
-{
+void LightingScene::init() {
 	// Enables lighting computations
 	glEnable(GL_LIGHTING);
 
 	// Sets up some lighting parameters
 	// Computes lighting only using the front face normals and materials
-	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);  
-	
+	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+
 	// Define ambient light (do not confuse with ambient component of individual lights)
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmbientLight);  
-	
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globalAmbientLight);
+
 	// Declares and enables two lights, with null ambient component
 
 	light0 = new CGFlight(GL_LIGHT0, light0_pos);
@@ -65,9 +63,9 @@ void LightingScene::init()
 
 	light1 = new CGFlight(GL_LIGHT1, light1_pos);
 	light1->setAmbient(ambientNull);
-	
+
 	light1->enable();
-	
+
 	// Added light2
 	light2 = new CGFlight(GL_LIGHT2, light2_pos);
 	light2->setAmbient(ambientNull);
@@ -88,27 +86,26 @@ void LightingScene::init()
 	light3->setKq(0.2);
 	light3->enable();
 	// Uncomment below to enable normalization of lighting normal vectors
-	glEnable (GL_NORMALIZE);
+	glEnable(GL_NORMALIZE);
 
 	//Declares scene elements
 	table = new myTable();
 	wall = new Plane();
 	boardA = new Plane(BOARD_A_DIVISIONS);
 	boardB = new Plane(BOARD_B_DIVISIONS);
-	
+
 	//Declares materials
-	materialA = new CGFappearance(ambA,difA,specA,shininessA);
-	materialB = new CGFappearance(ambB,difB,specB,shininessB);
+	materialA = new CGFappearance(ambA, difA, specA, shininessA);
+	materialB = new CGFappearance(ambB, difB, specB, shininessB);
 
 }
 
-void LightingScene::display() 
-{
+void LightingScene::display() {
 
 	// ---- BEGIN Background, camera and axis setup
-	
+
 	// Clear image and depth buffer everytime we update the scene
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Initialize Model-View matrix as identity (no transformation
 	glMatrixMode(GL_MODELVIEW);
@@ -119,7 +116,7 @@ void LightingScene::display()
 
 	light0->draw();
 	light1->draw();
-	
+
 	// Draw axis
 	axis.draw();
 
@@ -129,61 +126,73 @@ void LightingScene::display()
 
 	//First Table
 	glPushMatrix();
-		glTranslated(5,0,8);
-		table->draw();
+	glTranslated(5, 0, 8);
+	table->draw();
 	glPopMatrix();
 
 	//Second Table
 	glPushMatrix();
-		glTranslated(12,0,8);
-		table->draw();
+	glTranslated(12, 0, 8);
+	table->draw();
 	glPopMatrix();
 
 	//Floor
+	// Coefficients for material
+	float amb[3] = {0, 0,  0.3984};
+	float dif[3] = {0, 0,  0.3984};
+	float spec[3] = {0, 0,  0.1};
+	float shininess = 12.f;
+	CGFappearance *materialF = new CGFappearance(amb, dif, spec, shininess);
 	glPushMatrix();
-		glTranslated(7.5,0,7.5);
-		glScaled(15,0.2,15);
-		wall->draw();
+	materialF->apply();
+	glTranslated(7.5, 0, 7.5);
+	glScaled(15, 0.2, 15);
+	wall->draw();
 	glPopMatrix();
 
 	//LeftWall
-	glPushMatrix();
-		glTranslated(0,4,7.5);
-		glRotated(-90.0,0,0,1);
+	float amb1[3] = { 0.3984, 0, 0 };
+	float dif1[3] = { 0.3984, 0, 0};
+	float spec1[3] = { 0.20, 0, 0};
+	float shininess1 = 12.f;
+	CGFappearance *materialLw = new CGFappearance(amb1, dif1, spec1, shininess1);
 
-		glScaled(8,0.2,15);
-		wall->draw();
+	glPushMatrix();
+	materialLw->apply();
+	glTranslated(0, 4, 7.5);
+	glRotated(-90.0, 0, 0, 1);
+
+	glScaled(8, 0.2, 15);
+	wall->draw();
 	glPopMatrix();
 
 	//PlaneWall
 	glPushMatrix();
-		glTranslated(7.5,4,0);
-		glRotated(90.0,1,0,0);
-		glScaled(15,0.2,8);
-		wall->draw();
+	glTranslated(7.5, 4, 0);
+	glRotated(90.0, 1, 0, 0);
+	glScaled(15, 0.2, 8);
+	wall->draw();
 	glPopMatrix();
-
 
 	// Board A
 	glPushMatrix();
-		glTranslated(4,4,0.2);
-		glScaled(BOARD_WIDTH,BOARD_HEIGHT,1);
-		glRotated(90.0,1,0,0);
-		materialA->apply();
-		boardA->draw();
+	glTranslated(4, 4, 0.2);
+	glScaled(BOARD_WIDTH, BOARD_HEIGHT, 1);
+	glRotated(90.0, 1, 0, 0);
+	materialA->apply();
+	boardA->draw();
 	glPopMatrix();
-	
+
 	//PlaneB
 	glPushMatrix();
-		glTranslated(10.5,4,0.2);
-		glScaled(BOARD_WIDTH,BOARD_HEIGHT,1);
-		glRotated(90.0,1,0,0);
-		materialB->apply();
-		boardB->draw();
+	glTranslated(10.5, 4, 0.2);
+	glScaled(BOARD_WIDTH, BOARD_HEIGHT, 1);
+	glRotated(90.0, 1, 0, 0);
+	materialB->apply();
+	boardB->draw();
 	glPopMatrix();
-	
+
 	// ---- END Primitive drawing section
-	
 
 	// We have been drawing in a memory area that is not visible - the back buffer, 
 	// while the graphics card is showing the contents of another buffer - the front buffer
@@ -191,15 +200,14 @@ void LightingScene::display()
 	glutSwapBuffers();
 }
 
-LightingScene::~LightingScene() 
-{
-	delete(light0);
-	delete(light1);
+LightingScene::~LightingScene() {
+	delete (light0);
+	delete (light1);
 
-	delete(table);
-	delete(wall);
-	delete(boardA);
-	delete(boardB);
-	delete(materialA);
-	delete(materialB);
+	delete (table);
+	delete (wall);
+	delete (boardA);
+	delete (boardB);
+	delete (materialA);
+	delete (materialB);
 }
