01700acdd33742b6e2215f5555b193d318c4bc3c
André Freitas <p.andrefreitas@gmail.com>
2012-04-10 14:09:41 +0100

    Change Make output to cgra-tp5

--- tp5/code/nbproject/Package-Default.bash
+++ tp5/code/nbproject/Package-Default.bash
@@ -12,8 +12,8 @@ CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=MissingOutputInProject
-OUTPUT_BASENAME=MissingOutputInProject
+OUTPUT_PATH=bin/cgra-tp5
+OUTPUT_BASENAME=cgra-tp5
 PACKAGE_TOP_DIR=code/
 
 # Functions
@@ -59,7 +59,7 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/code"
+makeDirectory "${NBTMPDIR}/code/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
