3532572c9edf1649738186abb409e1e052405bdb
andrefreitas <p.andrefreitas@gmail.com>
2012-04-20 00:44:54 +0100

    Air Plane with aerodynamics equations and falling

--- tp5/code/include/MyPaperPlane.h
+++ tp5/code/include/MyPaperPlane.h
@@ -7,10 +7,14 @@ class MyPaperPlane:public CGFobject{
     double Xpos;
     double Ypos;
     double Zpos;
+    float scale;
+    bool collision;
+    float rotation;
     public:
     MyPaperPlane();
     void draw();
     void update(long milis);
+    void handleCollision(float milis);
 };
 
 #endif	/* MYPAPERPLANE_H */
--- tp5/code/src/MyPaperPlane.cpp
+++ tp5/code/src/MyPaperPlane.cpp
@@ -3,27 +3,28 @@
 #include "MyPaperPlane.h"
 #include "iostream"
 using namespace std;
+
 void MyPaperPlane::draw() {
-    float scale=0.2;
+    scale = 0.2;
     glPushMatrix();
-    glTranslated(Xpos,Ypos,Zpos);
-    glRotated(-90,0,1,0);
+    glTranslated(Xpos, Ypos, Zpos);
+    glRotated(rotation, 1, 0, 0);
     // The plane is composed by 3 triangles
     for (int i = 0; i < 3; i++) {
         glPushMatrix();
         // In this way we can draw a full paper plane with a cycle
-        glRotated(i*90,1,0,0);
-            // wing
-            glBegin(GL_TRIANGLES);
-            //Top (anti-clockwise)
-            glVertex3f(scale*5, 0, i==1?scale*2:scale*3);
-            glVertex3f(scale*5, 0, 0);
-            glVertex3f(0, 0, 0);
-            // Bottom (clockwise)
-            glVertex3f(0, 0, 0);
-            glVertex3f(scale*5, 0, 0);
-            glVertex3f(scale*5, 0, i==1?scale*2:scale*3);
-            glEnd();
+        glRotated(i * 90, 1, 0, 0);
+        // wing
+        glBegin(GL_TRIANGLES);
+        //Top (anti-clockwise)
+        glVertex3f(scale * 5, 0, i == 1 ? scale * 2 : scale * 3);
+        glVertex3f(scale * 5, 0, 0);
+        glVertex3f(0, 0, 0);
+        // Bottom (clockwise)
+        glVertex3f(0, 0, 0);
+        glVertex3f(scale * 5, 0, 0);
+        glVertex3f(scale * 5, 0, i == 1 ? scale * 2 : scale * 3);
+        glEnd();
         glPopMatrix();
     }
     glPopMatrix();
@@ -31,26 +32,73 @@ void MyPaperPlane::draw() {
 }
 
 MyPaperPlane::MyPaperPlane() {
+    bool collision = false;
+}
+
+void MyPaperPlane::update(long milis) {
+    // (1) get the seconds
+    float secondsT = milis / 1000.0;
+
+    // (2) cinematic variables (projectile movement with aerodynamics)
+    // see more here http://pt.wikipedia.org/wiki/Sustenta%C3%A7%C3%A3o_(aerodin%C3%A2mica)
+    float g = 9.8;
+
+    // angle of launching (rads)
+    float angleRad = 0.5;
+
+    // the velocity (m/s)
+    float velocity = 5; // the velocity vector norm
+    float v0x = velocity * cos(angleRad);
+    float v0y = velocity * sin(angleRad);
+    float vx = v0x;
+    float vy = v0y - 0.15 * secondsT; // fake aproximation cause this dependes of lift
+
+    // initial positions
+    float z0 = 7;
+    float y0 = 4;
+    float x0 = 10;
+
+    // lifts
+    float actualSpeed = (float) sqrt(vx * vx + vy * vy); // compute the norm
+    float kL = 0.35; // fake aproximmation
+    float arDensity = 1.225;
+    float wingArea = (5 * 3 / 2.0) * scale;
+    float lift = kL * arDensity / 2.0 * wingArea * actualSpeed*actualSpeed;
+
+    // forces
+    float forceY = g - lift;
+
+    // (3) cinematic equations (projectile movement)
+    Zpos = z0;
+    Ypos = y0;
+    Xpos = x0;
+
+    Zpos = z0;
+    Xpos = x0 - v0x*secondsT;
+    Ypos = y0 + v0y * secondsT - 1 / 2.0 * (forceY * secondsT * secondsT);
+    
+    // (4) debug info
+    cout << "y=" << Ypos << " x=" << Xpos << " velocity=" << vy << " m/s" << endl;
+        
+    // Colision detection
+    if ((Zpos < 0.1) | (Ypos < 0.1) | (Xpos < 0.1))
+        handleCollision(milis);
+
 
 }
 
- void MyPaperPlane::update(long milis){
-     // (1) get the seconds
-     float secondsT=milis/1000.0;
-     // (2) cinematic variables (projectile movement)
-     float v0=50; // m/s
-     float angle=30; // degrees 
-     long double g=0.000000000000000008; // m/s
-     float z0=7;
-     float y0=4;
-     // (3) cinematic equations
+void MyPaperPlane::handleCollision(float milis) {
+    //  get the seconds
+    float secondsT = milis / 1000.0;
+    float g = 1;
+    Zpos = 8;
+    Ypos = 5 - 1 / 2.0 * g * secondsT*secondsT;
+    rotation=180-180*1/(float)secondsT; // plane flip while falls
+    // collision
+    if (Ypos < 0) {
+        Ypos = 0.05;
+        rotation = 180; // flip over
+    }
+    Xpos = 0;
 
-     Xpos=5;
-     Zpos=z0-(v0*cos(angle)*secondsT); // is minus because the z-axis
-     cout << "DeltaZ: " << Zpos << "\n";
-     Ypos=y0+v0*sin(angle)*secondsT+(g*secondsT*secondsT)/(float)2 ;
-      cout << "DeltaY: " << Ypos << "\n";
-    
-     
-     
- }
+}
